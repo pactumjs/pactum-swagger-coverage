ac97f7c0407af116c70ad66621bce931b35cb1f3
ElnSorokina <51135612+ElnSorokina@users.noreply.github.com>
2023-11-21 16:35:13 +0000

    Add reportFile parameter for the ts users

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,6 +3,7 @@ import { name } from "."
 export let basePath: string;
 export let name: string;
 export let reportPath: string;
+export let reportFile: string;
 export let file: string;
 export let swaggerJsonUrl: string;
 export let swaggerYamlPath: string;
